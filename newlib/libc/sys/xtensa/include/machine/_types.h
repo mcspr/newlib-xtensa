1e5a34ccaec13c93826f1b591e8ddb13f9a7709d
Maxim Prokhorov <prokhorov.max@outlook.com>
2025-06-27 16:27:38 +0300

    machine/*.h override fixes
    upstream _default_types.h already provides a way to override stdint.h types with a macro
    similarly, use machine/types.h to provide lwip2 declaraction helper
    
    remove direct newlib sys/types.h changes
    
    amends 08a150b17c55de2c038d24697d1d2b73f3d0612f
    amends 034375125b6e6937831d0c9fd7c5ee4f28b48082

---
+++ newlib/libc/sys/xtensa/include/machine/_types.h
@@ -0,0 +1,19 @@
+/*
+ *  $Id$
+ */
+
+#ifndef _MACHINE__TYPES_H
+#define _MACHINE__TYPES_H
+
+// avoid gcc provided types, preserve signatures & printf formatting
+// ref. 08a150b17c55de2c038d24697d1d2b73f3d0612f
+#undef __INT16_TYPE__
+#define __INT16_TYPE__ short
+#undef __INT32_TYPE__
+#define __INT32_TYPE__ int
+#undef __INT64_TYPE__
+#define __INT64_TYPE__ long long
+
+#include <machine/_default_types.h>
+
+#endif

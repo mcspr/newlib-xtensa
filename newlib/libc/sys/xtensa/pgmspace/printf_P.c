210665cc1531fe804c2d6ec014d598c7c2d0e31d
Maxim Prokhorov <prokhorov.max@outlook.com>
2025-06-27 19:22:56 +0300

    Separate files for pgmspace, try to fix build issue with string/lib.a

---
+++ newlib/libc/sys/xtensa/pgmspace/printf_P.c
@@ -0,0 +1,13 @@
+/* Backwards compatibility, both format and arguments are allowed to be PSTR / PROGMEM */
+
+#include <stdio.h>
+
+int printf_P(const char* formatP, ...) {
+    int ret;
+    va_list arglist;
+    va_start(arglist, formatP);
+    ret = vprintf(formatP, arglist);
+    va_end(arglist);
+    return ret;
+}
+
---
+++ newlib/libc/sys/xtensa/pgmspace/strncasecmp_P.c
@@ -0,0 +1,48 @@
+/*
+ Modified from original source by Earle F. Philhower, III
+ Original authorship:
+ Copyright (c) 2015 Michael C. Miller.  All right reserved.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+// TODO - Optimize these routines to use 32-bit accesses whenever possible
+
+#define __need_size_t
+#include <stddef.h>
+
+#include <ctype.h>
+#include <string.h>
+#include <sys/pgmspace.h>
+
+int strncasecmp_P(const char* str1, const char* str2P, size_t size)
+{
+    int result = 0;
+
+    while (size > 0)
+    {
+        char ch1 = tolower(*str1++);
+        char ch2 = tolower(pgm_read_byte(str2P++));
+        result = ch1 - ch2;
+        if (result != 0 || ch2 == '\0')
+        {
+            break;
+        }
+
+        size--;
+    }
+
+    return result;
+}
